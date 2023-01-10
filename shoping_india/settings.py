3bb13fcb02081d3e4e4f46b88e5fe9fbd2be275e
krishna <krishnaaryan369@gmail.com>
2023-01-10 22:49:23 +0530

    krishna

--- shoping_india/settings.py
+++ shoping_india/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2eg%!=j!^rjfciv^l!5q@1dbhqr7r9u_6v6x9**wb(50+s1id=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh']
 
 
 # Application definition
@@ -128,3 +128,7 @@ EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS=os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build', 'static')
